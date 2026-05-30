9a8980fdd7ebcde65d94ba924fda7675360baec5
Jenny Danzmayr <mail@scientress.de>
2026-05-30 15:01:34 +0200

    geojson api endpoint

--- netbox_c3nav/api/views.py
+++ netbox_c3nav/api/views.py
@@ -1,4 +1,6 @@
 from netbox.api.viewsets import NetBoxModelViewSet
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .. import filtersets, models
 from .serializers import DevicePositionSerializer, OverlaySerializer
@@ -8,6 +10,14 @@ class DevicePositionViewSet(NetBoxModelViewSet):
     queryset = models.DevicePosition.objects.prefetch_related('device')
     serializer_class = DevicePositionSerializer
     filterset_class = filtersets.DevicePositionFilterSet
+
+    @action(detail=False, methods=['get'])
+    def as_geojson(self, request):
+        qs = self.filterset_class(request.GET, self.get_queryset(), request=request).qs
+        return Response({
+            'type': 'FeatureCollection',
+            'features': [dp.geojson for dp in qs],
+        })
 
 
 class OverlayViewSet(NetBoxModelViewSet):
