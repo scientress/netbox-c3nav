0bb15dd9aea8a8260cdf0bfb174ab92e58815289
Jenny Danzmayr <mail@scientress.de>
2026-05-31 05:51:00 +0200

    moved all css to scss file, vite build

--- netbox_c3nav/frontend/vite.config.ts
+++ netbox_c3nav/frontend/vite.config.ts
@@ -12,33 +12,31 @@ export default defineConfig({
   build: {
     sourcemap: true,
     outDir: '../static/netbox_c3nav/vite',
-    lib: {
-      entry: {
-        map: resolve(import.meta.dirname, 'src/map.ts')
-      },
-      name: 'netbox_c3nav',
-      formats: ['iife'],
-    },
     rolldownOptions: {
-      // input: {
-      //   main: resolve(import.meta.dirname, 'src/map.ts'),
-      // },
+      input: {
+        map: resolve(import.meta.dirname, 'src/map.ts'),
+        'map.css': resolve(import.meta.dirname, 'src/map.scss'),
+      },
       // make sure to externalize deps that shouldn't be bundled
       // external: ['leaflet'],
       output: {
+        // Filenames
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
         // Provide global variables to use in the UMD build for externalized deps
         globals: {
           leaflet: 'L',
         },
-        // codeSplitting: {
-        //   minSize: 1000,
-        //   groups: [
-        //     {
-        //       name: 'vendor',
-        //       test: /node_modules/,
-        //     },
-        //   ],
-        // },
+        codeSplitting: {
+          minSize: 1000,
+          groups: [
+            {
+              name: 'vendor',
+              test: /node_modules/,
+            },
+          ],
+        },
       }
     },
   },
