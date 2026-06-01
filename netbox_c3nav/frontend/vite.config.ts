435a7cc503b1c1e9e50d44a74d744bce392cfe0b
Jenny Danzmayr <mail@scientress.de>
2026-06-01 13:49:40 +0200

    vite: fixed asset urls in dev mode, improved in-lining config

--- netbox_c3nav/frontend/vite.config.ts
+++ netbox_c3nav/frontend/vite.config.ts
@@ -2,45 +2,62 @@ import { dirname, resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import {fileURLToPath, URL} from "node:url";
 
-export default defineConfig({
-  base: "./",
-  
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+export default defineConfig(({ command, mode, isSsrBuild, isPreview}) => {
+  const isDevServer: boolean = command === 'serve'
 
-  build: {
-    assetsInlineLimit: 0,
-    sourcemap: true,
-    outDir: '../static/netbox_c3nav/vite',
-    rolldownOptions: {
-      input: {
-        map: resolve(import.meta.dirname, 'src/map.ts'),
-        'map.css': resolve(import.meta.dirname, 'src/map.scss'),
+  return {
+    base: isDevServer ? '/' : './',
+
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
       },
-      // make sure to externalize deps that shouldn't be bundled
-      // external: ['leaflet'],
-      output: {
-        // Filenames
-        entryFileNames: '[name].js',
-        chunkFileNames: '[name].js',
-        assetFileNames: '[name].[ext]',
-        // Provide global variables to use in the UMD build for externalized deps
-        globals: {
-          leaflet: 'L',
-        },
-        codeSplitting: {
-          minSize: 1000,
-          groups: [
-            {
-              name: 'vendor',
-              test: /node_modules/,
-            },
-          ],
-        },
-      }
     },
-  },
+
+    build: {
+      assetsInlineLimit: (filePath: string, content: Buffer): boolean | undefined => {
+        // never inline leaflet assets
+        if (filePath.includes('leaflet/dist')) {
+          return false
+        }
+        // default logic for everything else
+        return undefined
+      },
+      sourcemap: true,
+      outDir: '../static/netbox_c3nav/vite',
+      rolldownOptions: {
+        input: {
+          map: resolve(import.meta.dirname, 'src/map.ts'),
+          'map.css': resolve(import.meta.dirname, 'src/map.scss'),
+        },
+        // make sure to externalize deps that shouldn't be bundled
+        // external: ['leaflet'],
+        output: {
+          // Filenames
+          entryFileNames: '[name].js',
+          chunkFileNames: '[name].js',
+          assetFileNames: '[name].[ext]',
+          // Provide global variables to use in the UMD build for externalized deps
+          globals: {
+            leaflet: 'L',
+          },
+          codeSplitting: {
+            minSize: 1000,
+            groups: [
+              {
+                name: 'vendor',
+                test: /node_modules/,
+              },
+            ],
+          },
+        }
+      },
+    },
+
+    server: {
+      port: 5173,
+      strictPort: true,
+      origin: 'http://localhost:5173',
+    }
+  }
 })
