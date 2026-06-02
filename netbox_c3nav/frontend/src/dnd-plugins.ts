b817376ac642faa500617e1cb42e4847e19bfdd1
Jenny Danzmayr <mail@scientress.de>
2026-06-02 18:50:48 +0200

    fixed cursor style over map
    lol it was just a typo, ffs

--- netbox_c3nav/frontend/src/dnd-plugins.ts
+++ netbox_c3nav/frontend/src/dnd-plugins.ts
@@ -1,8 +1,6 @@
 import {Plugin, PluginOptions} from '@dnd-kit/abstract'
 import {DragDropManager, StyleInjector} from "@dnd-kit/dom";
 
-// ToDo: For some reason this doesn't work, no idea why. Can't debug it. Style is added but ignored.
-// Note: This is probably because the dragged element is under the mouse.
 export class MapCursor extends Plugin<DragDropManager> {
   constructor(
     public manager: DragDropManager,
@@ -16,9 +14,8 @@ export class MapCursor extends Plugin<DragDropManager> {
     ) as StyleInjector | undefined;
 
     const unregisterStyles = styleInjector?.register(
-      `map { cursor: ${cursor} !important; }`
+      `#map { cursor: ${cursor} !important; }`
     );
-    console.log('map cursor added:', `* { cursor: ${cursor} !important; }`)
 
     if (unregisterStyles) {
       const originalDestroy = this.destroy.bind(this);
